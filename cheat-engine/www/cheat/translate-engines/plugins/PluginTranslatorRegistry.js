81fdb287eda49da77b87913082f9c43861d5f998
Areinu <areinu@gmail.com>
2026-05-19 23:20:15 +0200

    Add RecollectionModes MZ

--- cheat-engine/www/cheat/translate-engines/plugins/PluginTranslatorRegistry.js
+++ cheat-engine/www/cheat/translate-engines/plugins/PluginTranslatorRegistry.js
@@ -43,6 +43,8 @@ import { NuunSaveScreen3Translator } from './translators/NuunSaveScreen3Translat
 import { OriginMenuStatusTranslator } from './translators/OriginMenuStatusTranslator.js';
 import { PandaProgressTextWindowTranslator } from './translators/PandaProgressTextWindowTranslator.js';
 import { QuestSystemTranslator } from './translators/QuestSystemTranslator.js';
+import { RecollectionForMZTranslator } from './translators/RecollectionForMZTranslator.js';
+import { RecollectionModeMZTranslator } from './translators/RecollectionModeMZTranslator.js';
 import { SceneCustomMenuTranslator } from './translators/SceneCustomMenuTranslator.js';
 import { SceneGlossaryTranslator } from './translators/SceneGlossaryTranslator.js';
 import { SetMessageFontSizeTranslator } from './translators/SetMessageFontSizeTranslator.js';
@@ -117,6 +119,8 @@ class PluginTranslatorRegistry {
             PandaProgressTextWindowTranslator,
             PHQuestBookTranslator,
             QuestSystemTranslator,
+            RecollectionForMZTranslator,
+            RecollectionModeMZTranslator,
             SabaSimpleScenarioTranslator,
             SabaTachieTranslator,
             SceneCustomMenuTranslator,
